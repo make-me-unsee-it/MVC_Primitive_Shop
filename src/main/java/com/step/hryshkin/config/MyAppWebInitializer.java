433c1552b63e145ddf55ee05f879462a03a0a659
alekx <alekx-gr@yandex.by>
2022-08-10 19:16:48 +0300

    Initial commit

---
+++ src/main/java/com/step/hryshkin/config/MyAppWebInitializer.java
@@ -0,0 +1,28 @@
+package com.step.hryshkin.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+/**
+ * @author Alekx
+ */
+
+public class MyAppWebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
---
+++ src/main/java/com/step/hryshkin/controllers/HelloController.java
@@ -0,0 +1,18 @@
+package com.step.hryshkin.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author Alekx
+ */
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello-world")
+    public String sayHello() {
+        return "hello_world";
+    }
+}
+
