3e5e0dafcb7ecc93411a4027c874762c6cc3901a
alekx <alekx-gr@yandex.by>
2022-08-10 20:42:30 +0300

    alishev lesson 19 added

--- src/main/java/com/step/hryshkin/controllers/FirstController.java
+++ src/main/java/com/step/hryshkin/controllers/FirstController.java
@@ -3,6 +3,9 @@ package com.step.hryshkin.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Alekx
@@ -14,14 +17,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+        System.out.println("Hallo " + name + surname);
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
-    public String goodbyePage(){
+    public String goodbyePage() {
         return "first/goodbye";
     }
-
-
 }
